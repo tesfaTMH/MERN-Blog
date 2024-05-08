0173ab7f30a12f4539ce69b7c85cf4e44989ef36
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-05-08 18:24:07 +0300

    react and tailwind css installed and first initial template created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="text-3xl font-bold underline">Hello from frontend</h2>
+    </>
+  );
+}
+
+export default App;
