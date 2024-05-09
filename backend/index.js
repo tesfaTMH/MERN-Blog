d48035624d5e71774eaa1a8072a5f75e15257d35
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-05-09 08:35:07 +0300

    Connection to DB done

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    console.log(`Successfully connected to MONGODB: `)
+}).catch((err) =>{
+    console.log(err)
+})
 
 const app = express()
 const PORT = 3000
