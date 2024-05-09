236621151ab2b8bd1495788278799878cc868c78
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-05-09 08:09:48 +0300

    Backend skeleton created

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -38,7 +38,9 @@ const Header = () => {
             <FaMoon />
           </Button>
           <Link to="/signin">
-            <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign In
+            </Button>
           </Link>
         </div>
 
